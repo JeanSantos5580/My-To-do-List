18366a7377001520980e64e76952a6e3aa31ebe2
Jean Santos <dev.jean.ap@gmail.com>
2023-03-07 16:33:09 -0300

    feature: create task svg indicating to user that there isn't any task

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 const buttonCreateNewTask = document.querySelector(".button-new-task");
 const buttonFinishTask = document.querySelector(".container-checkbox");
+const containerModal = document.querySelector(".container-modal-new-task");
+const inputsModal = containerModal.querySelectorAll("input");
 const modalButtonCancel = document.querySelector(".button-cancel");
 const modalButtonAdd = document.querySelector(".button-add");
 const backgroundModal = document.querySelector(".background-modal");
 const containerModalNewTask = document.querySelector(
   ".container-modal-new-task"
 );
+const containerCards = document.querySelector(".container-cards");
+const childrenContainerCards = containerCards.children;
+const containerImgWaitingTask = document.querySelector(
+  ".container-img-waiting-task"
+);
 
 let taskList = [];
+
+const checkForTasks = () => {
+  for (let i = 0; i < childrenContainerCards.length; i++) {
+    if (childrenContainerCards[i].classList.contains("new-card-generated")) {
+      containerImgWaitingTask.style.display = "none";
+    }
+
+    if(!(childrenContainerCards[i].classList.contains("new-card-generated"))){
+      containerImgWaitingTask.style.display = "flex";
+    }
+  }
+};
 
 const openModalNewTask = () => {
   backgroundModal.style.visibility = "visible";
@@ -26,7 +45,6 @@ const addnewTaskCard = (
   taskStart,
   taskEnd
 ) => {
-
   /* Create new task */
   const containerCards = document.querySelector(".container-cards");
   const taskCard = document.querySelector(".new-task-card");
@@ -52,36 +70,50 @@ const addnewTaskCard = (
   console.log(taskList);
 
   containerCards.appendChild(newCard);
+  newCard.style.display = "flex";
+
+  /* Check if there's at least a task, if true, remove the wait image */
+  newCard.classList.add("new-card-generated");
+  checkForTasks();
 
   /* Delete task */
   const deleteTask = newCard.querySelector(".container-icon-xmark");
   deleteTask.addEventListener("click", (e) => {
     newCard.remove();
+    checkForTasks();
   });
 
   /* Finish task */
   const checkbox = newCard.querySelector(".checkbox");
-  const textDecoration = "line-through #e63946 3px"
+  const textDecoration = "line-through #ffbe0b 3px";
   const inputsValues = [
-    newTaskTitle, 
-    newTaskDescription, 
-    newTaskDate, 
-    newTaskRangeStart, 
-    newTaskRangeEnd
-  ]
+    newTaskTitle,
+    newTaskDescription,
+    newTaskDate,
+    newTaskRangeStart,
+    newTaskRangeEnd,
+  ];
 
   checkbox.addEventListener("click", (e) => {
-    if(checkbox.checked){
-      for(let inputValue of inputsValues){
+    if (checkbox.checked) {
+      for (let inputValue of inputsValues) {
         inputValue.style.textDecoration = textDecoration;
       }
-    }
-    else {
-      for(let inputValue of inputsValues){
+    } else {
+      for (let inputValue of inputsValues) {
         inputValue.style.textDecoration = "none";
       }
     }
   });
+
+  /* Clear inputs */
+  if (modalButtonAdd) {
+    taskTitle.value = "";
+    taskDescription.value = "";
+    taskDate.value = "";
+    taskStart.value = "";
+    taskEnd.value = "";
+  }
 };
 
 const takeFormData = () => {
