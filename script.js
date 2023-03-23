574fffc210dcdefb2219fb4f06a5266c019af03d
Jean Santos <dev.jean.ap@gmail.com>
2023-03-23 15:08:31 -0300

    chore: Rendering logic adjustments for task date and task range elements

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const buttonCreateNewTask = document.querySelector(".button-new-task");
-const buttonFinishTask = document.querySelector(".container-checkbox");
 const modalButtonCancel = document.querySelector(".button-cancel");
 const modalButtonAdd = document.querySelector(".button-add");
 const backgroundModal = document.querySelector(".background-modal");
 const containerModalNewTask = document.querySelector(
   ".container-modal-new-task"
 );
-
 const containerCards = document.querySelector(".container-cards");
 const childrenContainerCards = containerCards.children;
 const containerImgWaitingTask = document.querySelector(
@@ -17,11 +15,14 @@ let taskList = [];
 
 const checkForTasks = () => {
   for (let i = 0; i < childrenContainerCards.length; i++) {
-    if (childrenContainerCards[i].classList.contains("new-card-generated")) {
+    const containsNewClass =
+      childrenContainerCards[i].classList.contains("new-card-generated");
+
+    if (containsNewClass) {
       containerImgWaitingTask.style.display = "none";
     }
 
-    if (!childrenContainerCards[i].classList.contains("new-card-generated")) {
+    if (!containsNewClass) {
       containerImgWaitingTask.style.display = "flex";
     }
   }
@@ -46,8 +47,12 @@ const addnewTaskCard = (
 ) => {
   /* Create new task */
   const containerCards = document.querySelector(".container-cards");
-  const taskCard = document.querySelector(".new-task-card");
+  const taskCard = containerCards.querySelector(".new-task-card");
   const newCard = taskCard.cloneNode(true);
+  const deleteTask = newCard.querySelector(".container-icon-xmark");
+  const checkbox = newCard.querySelector(".checkbox");
+  const textDecoration = "line-through #06d6a0 3px";
+  const fontColor = "#aaa";
 
   const newTaskTitle = newCard.querySelector(".task-title");
   newTaskTitle.innerHTML = taskTitle;
@@ -56,35 +61,61 @@ const addnewTaskCard = (
   newTaskDescription.innerHTML = taskDescription;
 
   const newTaskDate = newCard.querySelector(".task-date");
-  newTaskDate.innerHTML = taskDate;
+
+  const containerTaskRange = newCard.querySelector(".container-task-range")
 
   const newTaskRangeStart = newCard.querySelector(".task-range-start");
-  newTaskRangeStart.innerHTML = taskStart;
-
+  const containerRangeStart = newCard.querySelector(".container-range-start")
+  
   const newTaskRangeEnd = newCard.querySelector(".task-range-end");
-  newTaskRangeEnd.innerHTML = taskEnd;
+  const containerRangeEnd = newCard.querySelector(".container-range-end")
+
+  /* Check if inputs of task date creation and task range values are true or false */
+
+  const checkDateAndHourRange = () => {
+    taskDate
+      ? (newTaskDate.innerHTML = taskDate)
+      : (newTaskDate.style.display = "none");
+
+    taskStart
+      ? (newTaskRangeStart.innerHTML = taskStart)
+      : (containerRangeStart.style.display = "none");
+
+    taskEnd
+      ? (newTaskRangeEnd.innerHTML = taskEnd)
+      : (containerRangeEnd.style.display = "none");
+
+    (!taskStart && !taskEnd) && (containerTaskRange.style.display = "none");
+  };
+
+  checkDateAndHourRange();
+
+  /* Add the new card into array taskList*/
 
   taskList.push(newCard);
 
-  /* console.log(taskList); */
 
+  /* Add the new card as a child of container cards */
   containerCards.appendChild(newCard);
+  containerCards.
+
+  /* Changes display propertie of the new card */
   newCard.style.display = "flex";
 
-  /* Check if there's at least a task, if true, remove the wait image */
+  /* After the new card is created a new class is added to it */
   newCard.classList.add("new-card-generated");
+
+  /* Check if there's at least a task, if true, remove the wait image */
   checkForTasks();
 
-  /* Delete task */
-  const deleteTask = newCard.querySelector(".container-icon-xmark");
+  /* Delete task, and check if after this, still there's any task card */
   deleteTask.addEventListener("click", (e) => {
     newCard.remove();
     checkForTasks();
   });
 
   /* Finish task */
-  const checkbox = newCard.querySelector(".checkbox");
-  const textDecoration = "line-through #ffbe0b 3px";
+
   const inputsValues = [
     newTaskTitle,
     newTaskDescription,
@@ -97,10 +128,12 @@ const addnewTaskCard = (
     if (checkbox.checked) {
       for (let inputValue of inputsValues) {
         inputValue.style.textDecoration = textDecoration;
+        inputValue.style.color = fontColor;
       }
     } else {
       for (let inputValue of inputsValues) {
         inputValue.style.textDecoration = "none";
+        inputValue.style.color = "";
       }
     }
   });
@@ -108,7 +141,9 @@ const addnewTaskCard = (
 
 const takeFormData = () => {
   let inputTaskTitle = document.querySelector(".input-task-title").value;
-  let inputTaskDescription = document.querySelector(".input-task-description").value;
+  let inputTaskDescription = document.querySelector(
+    ".input-task-description"
+  ).value;
   let inputTaskDate = document.querySelector(".input-task-date").value;
   let inputTimeRangeStart = document.querySelector(".range-start").value;
   let inputTimeRangeEnd = document.querySelector(".range-end").value;
@@ -120,14 +155,14 @@ const takeFormData = () => {
     inputTimeRangeStart,
     inputTimeRangeEnd
   );
-  
+
   /* Clear form for user to create a new task */
 
-  document.querySelector(".input-task-title").value = ''
-  document.querySelector(".input-task-description").value = ''
-  document.querySelector(".input-task-date").value = ''
-  document.querySelector(".range-start").value = ''
-  document.querySelector(".range-end").value = ''
+  document.querySelector(".input-task-title").value = "";
+  document.querySelector(".input-task-description").value = "";
+  document.querySelector(".input-task-date").value = "";
+  document.querySelector(".range-start").value = "";
+  document.querySelector(".range-end").value = "";
 };
 
 buttonCreateNewTask.addEventListener("click", (e) => openModalNewTask());
